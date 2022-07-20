9c1a5d404fd17c5684460afc57764e3aab1b0474
“natalialionti” <nataliasaxo@gmail.com>
2022-07-20 19:06:17 -0300

    1º commit parte5

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 3030;
 
 app.listen (port, () => console.log("Servidor corriendo en puerto " + port));
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "views", "home.html"));});
+app.get("/register", (req, res) => {res.sendFile(path.join(__dirname, "views/register.html"));});
+
    
 app.use(express.static("public"));  
 
